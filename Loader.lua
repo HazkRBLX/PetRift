93aad96161f36e6979bb0a1a71ff89fcaef67d81
Hazk <130129643+T1deHub@users.noreply.github.com>
2023-04-07 18:56:04 -0700

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,9 @@
+
+--// Tide Hub
+if game.PlaceId == 11448052802 then
+    print("Pet Rift")
+    loadstring(game:HttpGet('https://pastebin.com/raw/mg61uG7y'))()
+    elseif game.PlaceId == 8540346411 then
+    print("Rebirth Champions X")
+    loadstring(game:HttpGet('https://pastebin.com/raw/nD5WkndH'))()
+    end
