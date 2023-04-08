38d88d7c2619c8acf03647f55bc9f7063390a977
Hazk <130129643+T1deHub@users.noreply.github.com>
2023-04-07 19:06:22 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,5 +5,5 @@ print("Pet Rift")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/T1deHub/Scripts/main/PetRift.lua"))()
 elseif game.PlaceId == 8540346411 then
 print("Rebirth Champions X")
-loadstring(game:HttpGet('https://pastebin.com/raw/nD5WkndH'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/T1deHub/Scripts/main/RebirthChampionsX.lua'))()
 end
