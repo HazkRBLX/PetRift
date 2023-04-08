47c945124009c06add3089e2fd06fa218463e2fb
Hazk <130129643+T1deHub@users.noreply.github.com>
2023-04-07 18:58:55 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 
 --// Tide Hub
 if game.PlaceId == 11448052802 then
-    print("Pet Rift")
-    loadstring(game:HttpGet('https://pastebin.com/raw/mg61uG7y'))()
-    elseif game.PlaceId == 8540346411 then
-    print("Rebirth Champions X")
-    loadstring(game:HttpGet('https://pastebin.com/raw/nD5WkndH'))()
-    end
+print("Pet Rift")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/T1deHub/Scripts/main/PetRift.lua"))()
+elseif game.PlaceId == 8540346411 then
+print("Rebirth Champions X")
+loadstring(game:HttpGet('https://pastebin.com/raw/nD5WkndH'))()
+end
